ec21edbd12290cc7aba558439867abc6243259d3
Kousik Dasari <kousik@apxor.com>
2026-01-28 15:50:23 +0530

    chore: initialize Swift Package and added Products

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "ApxorSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "ApxorSDK", targets: ["ApxorSDK"]),
+        .library(name: "APXContextEvaluator", targets: ["APXContextEvaluator"]),
+        .library(name: "APXRTAPlugin", targets: ["APXRTAPlugin"]),
+        .library(name: "APXWYSIWYGPlugin", targets: ["APXWYSIWYGPlugin"]),
+        .library(name: "APXSurveyPlugin", targets: ["APXSurveyPlugin"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ApxorSDK",
+            url: "https://repo.apxor.com/artifactory/libs-release-ios/core/21044/ApxoriOSSDK-Core-21044.zip",
+            checksum: "299e4ba6c48d5159b4bd6db1193f1dac4a9fdbc43248b100a641adc82c478570"
+        ),
+        .binaryTarget(
+            name: "APXContextEvaluator",
+            url: "https://repo.apxor.com/artifactory/libs-release-ios/ce/10531/ApxoriOSSDK-CE-10531.zip",
+            checksum: "4a465a502088478e26e248df71808a5657363b8a60b6cec69d335854f72d7f76"
+        ),
+        .binaryTarget(
+            name: "APXRTAPlugin",
+            url: "https://repo.apxor.com/artifactory/libs-release-ios/rta/10949/ApxoriOSSDK-RTA-10949.zip",
+            checksum: "0a67b6310340254ee3baf76d25964d1b0bd94815d7bb5822317ab5fa46861702"
+        ),
+        .binaryTarget(name: "APXWYSIWYGPlugin",
+            url: "https://repo.apxor.com/artifactory/libs-release-ios/wysiwyg/10275/ApxoriOSSDK-WYSIWYG-10275.zip",
+            checksum: "b16bee6d93c750e586b3b999a8afd4e7b4786e4d94374fd728e41ac0b1ff09d7"
+        ),
+        .binaryTarget(name: "APXSurveyPlugin",
+            url: "https://repo.apxor.com/artifactory/libs-release-ios/survey/10424/ApxoriOSSDK-Survey-10424.zip",
+            checksum: "83d0caf7ca9d13d9378a448c425c84659ff506d222d3726cd1956a1991e400be"
+        )
+    ]
+)
