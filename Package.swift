9e51aed2ec35b6e5daeb3f204785111cf3bd8fb6
Kousik Dasari <kousik@apxor.com>
2026-02-26 12:18:30 +0530

    bump: updated core version to v21045

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ApxorSDK",
-            url: "https://repo.apxor.com/artifactory/libs-release-ios/core/21044/ApxoriOSSDK-Core-21044.zip",
-            checksum: "299e4ba6c48d5159b4bd6db1193f1dac4a9fdbc43248b100a641adc82c478570"
+            url: "https://repo.apxor.com/artifactory/libs-release-ios/core/21045/ApxoriOSSDK-Core-21045.zip",
+            checksum: "f39a72f11263658d5c450c807826a3df43e8bb08683e97ff47481f70190d34b9"
         ),
         .binaryTarget(
             name: "APXContextEvaluator",
